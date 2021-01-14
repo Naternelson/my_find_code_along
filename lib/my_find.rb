dcb70d16b9246f2633fb72df88b434ada191f91d
Nate <7nater.nelson@gmail.com>
2021-01-14 15:06:33 -0700

    commit tree 9a47f4e1f6f4e64f6cc30462874f8cde912253fa

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
-def my_find(collection)
-
+def my_find(collection) #Buiding a method that mimics the #find method
+    i = 0
+    array = []
+    while i < collection.length
+        return collection[i] if yield(collection[i]) 
+        i+=1
+    end
 end
